fe5bc2690110109d0a7ef324dfe43bb6b5c2f507
Harry Ng <harryworld@gmail.com>
2014-06-14 14:14:29 +0800

    Add GET request

--- src/request/kodw/MainActivity.java
+++ src/request/kodw/MainActivity.java
@@ -1,6 +1,6 @@
 package request.kodw;
 
-import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
@@ -10,7 +10,14 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.royalnext.base.activity.BaseActivity;
+import com.royalnext.base.util.network.ApiCache;
+import com.royalnext.base.util.network.ServerTaskListener;
+import com.royalnext.base.util.network.ServerTaskManager;
+
+import java.util.HashMap;
+
+public class MainActivity extends BaseActivity {
 
     private WebView webView;
     private TextView scheduleView;
@@ -19,14 +26,35 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        super.onCreate(savedInstanceState, R.layout.activity_main);
         // Start coding here
 
         // Generate deviceId
         deviceId = Settings.Secure.getString(getBaseContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        Log.d("KODW", "Device ID: " + deviceId);
 
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("deviceId", deviceId);
+        params.put("username", deviceId);
+        params.put("appId", "jnFcGCugEqTXpTu66");
+
+        stm.startTask("http://api.homesmartly.com:8080/collectionapi/members", params, new ServerTaskListener() {
+            @Override
+            public void onStart() {
+            }
+
+            @Override
+            public void onSuccess(String result) {
+                Log.d("KODW", "Success: " + result);
+            }
+
+            @Override
+            public void onError(String result, String code, String msg) {
+                Log.d("KODW", "Error code: " + code + " / " + msg);
+            }
+        }, ApiCache.FLAG_NO_CACHE, ServerTaskManager.GET_TYPE, null);
+
+        // Shared WebView
         webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -37,6 +65,18 @@ public class MainActivity extends Activity {
                 Log.d("Debug", url);
                 webView.loadUrl(url);
                 return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                showLoading();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                hideLoading();
             }
         });
         webView.loadUrl("http://mobile.kodw.org/");
---
+++ src/request/kodw/app/App.java
@@ -0,0 +1,9 @@
+package request.kodw.app;
+
+import com.royalnext.base.app.BaseApp;
+
+/**
+ * Created by harryng on 14/6/14.
+ */
+public class App extends BaseApp {
+}
